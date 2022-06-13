140ec1d941cdcc5b1c65b920095696276c9a7d9c
Owen <ohutchyhutch@gmail.com>
2022-06-13 15:11:38 -0400

    Updating Dependencies

--- utils/users.js
+++ utils/users.js
@@ -1,25 +1,26 @@
 const users = [];
 
 // Join user to room
-function userJoin(id, username, room){
-    const user = { id, username, room};
+function userJoin(id, username, room) {
+    const user = { id, username, room };
 
     users.push(user);
 
     return user;
 }
 
-function getCurrentUser(id){
+function getCurrentUser(id) {
     return users.find(user => user.id === id);
 }
 function userLeave(id) {
+    //If user leaves and was the original user, make the next joined user the new original user.
     const index = users.findIndex(user => user.id === id);
 
-    if (index !== -1){
-        return users.splice(index,1)[0];
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
     }
 }
-function getRoomUsers(room){
+function getRoomUsers(room) {
     return users.filter(user => user.room === room);
 }
 module.exports = {
